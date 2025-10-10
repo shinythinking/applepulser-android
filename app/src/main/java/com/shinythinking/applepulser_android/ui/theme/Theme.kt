3ec8ecb89c1faa10f0d1dc45b26ad2778d8c340c
shinythinking <shinythink@gmail.com>
2025-10-10 09:10:54 +0900

    design: Set the minimum Theme

--- app/src/main/java/com/shinythinking/applepulser_android/ui/theme/Theme.kt
+++ app/src/main/java/com/shinythinking/applepulser_android/ui/theme/Theme.kt
@@ -1,18 +1,16 @@
 package com.shinythinking.applepulser_android.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
+    primary = ButtonGrey,
+    onPrimary = White,
+    secondary = White,
+    onSecondary = Black,
     tertiary = Pink80
 )
 
@@ -39,15 +37,16 @@ fun Applepulser_androidTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = DarkColorScheme
+//        when {
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
+//
+//        darkTheme -> DarkColorScheme
+//        else -> DarkColorScheme
+//    }
 
     MaterialTheme(
         colorScheme = colorScheme,
